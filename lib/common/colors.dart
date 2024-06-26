aa8e4741bc847348ec519c016cf6a5ff1cc1644c
DhanyaVivek <105973523+DhanyaVivek@users.noreply.github.com>
2024-06-26 17:39:42 +0530

    Add files via upload

---
+++ lib/common/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const themeColor = Colors.white;
+const appBarColor = Color(0xFF0949d6);
+const menutextColor=Colors.black;
+const bgGridColor=Colors.grey;
